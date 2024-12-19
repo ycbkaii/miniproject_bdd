156e80cd23b8bb9795c0b56a56cbe47be904262f
ycbkaii <yanis.chiouar@gmail.com>
2024-12-19 14:32:12 +0100

    Avancement miniProjet

--- analyse.py
+++ analyse.py
@@ -20,7 +20,6 @@ countTitle = data.groupby("Director").count().reset_index()[["Title","Director"]
 sizes = []
 for i in range(len(labels)) :
     sizes.append(countTitle.loc[countTitle["Director"] == labels[i] ]["Title"].to_list()[0])
-print(sizes)
 
 for j in range(len(labels)) :
     labels[j] = labels[j].replace(" ","\n")
@@ -31,12 +30,11 @@ bar  = plt.bar(labels,sizes,width=0.5)
 plt.title('Répartition des réalisateur')
 plt.xlabel("Catégories")
 plt.ylabel("Valeurs")
-plt.show()
+# plt.show()
 
 #endregion
 
 #region BARGRAPH
-raise
 # On fait un BARGRAPH mentionnant la somme du budget de chaque auteurs sur chacuns de leurs  films et qui ont au moins un film qui est supérieur à 9 en average rating
 
 dataBarGraph = data[['Director', 'Budget', 'Average Rating']]
@@ -45,12 +43,20 @@ dataBarGraph = data[['Director', 'Budget', 'Average Rating']]
 directorsSelected = dataBarGraph.loc[data['Average Rating'] >= 9.0]["Director"].to_list()
 
 
-# FOnction pour convertir le budget en dollars
+
+
+
+
+
+
+# Fonction pour convertir le budget en dollars
 def convertNumber(row) :
     input_string = row
-    
+    if (str(input_string) == "nan" ) :
+        return 0
     # Table des taux de change (symboles -> taux en USD)
     exchange_rates = {
+        '$' : 1.00,
         '€': 1.10,    # Euro
         '£': 1.25,    # Livre sterling
         'C$': 0.75,   # Dollar canadien
@@ -65,11 +71,27 @@ def convertNumber(row) :
         'R': 0.05,    # Rand sud-africain
         '₩': 0.00075, # Won sud-coréen
         '₱': 0.018,   # Peso philippin
-        'RM': 0.21    # Ringgit malaisien
+        'RM': 0.21,    # Ringgit malaisien
+        'DEM' : 0.562,
+        'ROL' : 0.000022
     }
     
+    
+    
+    # Fonction pour vérifier si une chaîne contient une devise
+    def contains_currency(string):
+        currency_regex = re.compile(r'(' + '|'.join(re.escape(key) for key in exchange_rates.keys()) + r')')
+        match = currency_regex.search(string)
+        return match.group(0) if match else None
+    
+    contientDevise = contains_currency(input_string)
+    if contientDevise == None :
+        return 0
+    
     # On convertit en dollars
-    devise = input_string
+    devise = exchange_rates[contientDevise]
+    
+    
     
     
     # Utilise une expression régulière pour capturer les chiffres, y compris les séparateurs de milliers
@@ -77,20 +99,38 @@ def convertNumber(row) :
     if match:
         # Remplace les virgules pour convertir en un nombre entier
         number = int(match.group(0).replace(',', ''))
-        return number
+        return number * devise
     else:
-        return None  # Retourne None si aucun nombre n'est trouvé
+        return 0  # Retourne None si aucun nombre n'est trouvé
 
 # La somme du budget de chacuns des réalisateurs
-sumBudgetDirector = ...
+dataBarGraph.loc[:, "Budget"] = dataBarGraph["Budget"].apply(convertNumber)
 
+sommeBudget = dataBarGraph.groupby("Director").sum().reset_index()
+
+sizesDirector = []
+labelsDirector = []
+
+for elem in directorsSelected :
+   budgetDirector = sommeBudget.loc[sommeBudget["Director"] == elem]
+   budget = budgetDirector["Budget"].to_list()[0]
+   label = budgetDirector["Director"].to_list()[0]
+   if int(budget) > 0 :
+    sizesDirector.append(budget)
+    labelsDirector.append(label)
+       
 
 # Création du diagramme en secteurs
 plt.figure(figsize=(8, 8))
-plt.bar(labels,sizes)
+plt.bar(labelsDirector,sizesDirector)
 plt.title('Répartition des réalisateur')
 plt.xlabel("Catégories")
 plt.ylabel("Valeurs")
 plt.show()
 
 #endregion
+
+#region DECISIONNEL
+
+sommeBudgetTrier = sommeBudget.sort_values(by="Budget", ascending=False).loc[sommeBudget["Director"] == "Christopher Nolan"])
+print(sommeBudgetTrier)
